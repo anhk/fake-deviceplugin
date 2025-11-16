752c3f5fd6b9151139909a2fdd3f68d8a0427cab
anhongkui <anhongkui@jd.com>
2025-11-16 18:16:32 +0800

    score

--- app/scheduler/scheduler.go
+++ app/scheduler/scheduler.go
@@ -42,15 +42,25 @@ func (sched *Schedueler) Filter(c *gin.Context) {
 }
 
 func (sched *Schedueler) Score(c *gin.Context) {
-	// var args schedulerapi.ExtenderArgs
-	// if err := c.ShouldBindJSON(&args); err != nil {
-	// 	c.JSON(400, gin.H{"error": err.Error()})
-	// 	return
-	// }
+	var request schedulerapi.ExtenderArgs
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 
-	// var result schedulerapi.ExtenderScoreResult
-	// c.JSON(http.StatusOK, &result)
+	var result schedulerapi.HostPriorityList
+	if request.NodeNames != nil {
+		for _, nodeName := range *request.NodeNames {
+			result = append(result, schedulerapi.HostPriority{Host: nodeName, Score: 10})
+		}
+	} else if request.Nodes != nil {
+		for _, node := range request.Nodes.Items {
+			result = append(result, schedulerapi.HostPriority{Host: node.Name, Score: 10})
+		}
+	}
+	log.Debug("Scheduler Score called, request: %s, response: %s", utils.JsonString(request), utils.JsonString(result))
 
+	c.JSON(http.StatusOK, &result)
 }
 
 func (sched *Schedueler) Bind(c *gin.Context) {
